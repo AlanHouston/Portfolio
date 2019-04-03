2c68244f8eaf59e82d9c50e16efed0ec3b1fd7d1
Jon Woo <jw56578@gmail.com>
2019-04-03 18:13:02 -0500

    stuff

---
+++ precache-manifest.d9089f199ab4d0471727847210f2304b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "114562180dc024aa8323",
+    "url": "/static/js/main.11456218.chunk.js"
+  },
+  {
+    "revision": "3e6cdb3e4bf8e79a2206",
+    "url": "/static/js/2.3e6cdb3e.chunk.js"
+  },
+  {
+    "revision": "114562180dc024aa8323",
+    "url": "/static/css/main.7eef13f7.chunk.css"
+  },
+  {
+    "revision": "3e6cdb3e4bf8e79a2206",
+    "url": "/static/css/2.ef1a276b.chunk.css"
+  },
+  {
+    "revision": "8cb16b5a76e4d599f74f5ddf250cb346",
+    "url": "/index.html"
+  }
+];
